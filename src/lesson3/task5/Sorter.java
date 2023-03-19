7af205cc9e5a041bfed5d0e511d3e41230119c91
DeafMist <romapr70@gmail.com>
2023-03-19 21:34:32 +0300

    Get some refactor to avoid mistakes

--- src/lesson3/task5/Sorter.java
+++ src/lesson3/task5/Sorter.java
@@ -15,19 +15,23 @@ public class Sorter {
         Files.deleteIfExists(sortedFile.toPath());
         Files.copy(dataFile.toPath(), sortedFile.toPath());
 
-        long fileLength = getFileLength(dataFile);
+        long fileLength = getFileLength(sortedFile);
 
         long curUnit = 1;
         while (curUnit < fileLength) {
+            long file1Length = 0;
+            long file2Length = 0;
             try (Scanner scanner = new Scanner(new FileInputStream(sortedFile));
                  PrintWriter pw1 = new PrintWriter(file1);
                  PrintWriter pw2 = new PrintWriter(file2)) {
                 while (scanner.hasNextLong()) {
                     for (long i = 0; i < curUnit && scanner.hasNextLong(); i++) {
                         pw1.println(scanner.nextLong());
+                        file1Length++;
                     }
                     for (long i = 0; i < curUnit && scanner.hasNextLong(); i++) {
                         pw2.println(scanner.nextLong());
+                        file2Length++;
                     }
                 }
                 pw1.flush();
@@ -37,45 +41,65 @@ public class Sorter {
             try (PrintWriter pw = new PrintWriter(sortedFile);
                  Scanner scanner1 = new Scanner(new FileInputStream(file1));
                  Scanner scanner2 = new Scanner(new FileInputStream(file2))) {
-                long a1 = scanner1.nextLong();
-                long a2 = scanner2.nextLong();
-                while (scanner1.hasNextLong() && scanner2.hasNextLong()) {
+                long a1 = Long.MAX_VALUE;
+                long a2 = Long.MAX_VALUE;
+
+                long curPosFile1 = 0;
+                long curPosFile2 = 0;
+
+                if (scanner1.hasNextLong()) {
+                    a1 = scanner1.nextLong();
+                    curPosFile1++;
+                }
+
+                if (scanner2.hasNextLong()) {
+                    a2 = scanner2.nextLong();
+                    curPosFile2++;
+                }
+
+                while (curPosFile1 <= file1Length && curPosFile2 <= file2Length) {
                     long i = 0;
                     long j = 0;
-                    while (i < curUnit && j < curUnit && scanner1.hasNextLong() && scanner2.hasNextLong()) {
+                    while (i < curUnit && j < curUnit && curPosFile1 <= file1Length && curPosFile2 <= file2Length) {
                         if (a1 < a2) {
                             pw.println(a1);
-                            a1 = scanner1.nextLong();
+                            if (scanner1.hasNextLong()) a1 = scanner1.nextLong();
+                            curPosFile1++;
                             i++;
                         }
                         else {
                             pw.println(a2);
-                            a2 = scanner2.nextLong();
+                            if (scanner2.hasNextLong()) a2 = scanner2.nextLong();
+                            curPosFile2++;
                             j++;
                         }
                     }
 
-                    while (i < curUnit && scanner1.hasNextLong()) {
+                    while (i < curUnit && curPosFile1 <= file1Length) {
                         pw.println(a1);
-                        a1 = scanner1.nextLong();
+                        if (scanner1.hasNextLong()) a1 = scanner1.nextLong();
+                        curPosFile1++;
                         i++;
                     }
 
-                    while (j < curUnit && scanner2.hasNextLong()) {
+                    while (j < curUnit && curPosFile2 <= file2Length) {
                         pw.println(a2);
-                        a2 = scanner2.nextLong();
+                        if (scanner2.hasNextLong()) a2 = scanner2.nextLong();
+                        curPosFile2++;
                         j++;
                     }
                 }
 
-                while (scanner1.hasNextLong()) {
+                while (curPosFile1 <= file1Length) {
                     pw.println(a1);
-                    a1 = scanner1.nextLong();
+                    curPosFile1++;
+                    if (scanner1.hasNextLong()) a1 = scanner1.nextLong();
                 }
 
-                while (scanner2.hasNextLong()) {
+                while (curPosFile2 <= file2Length) {
                     pw.println(a2);
-                    a2 = scanner2.nextLong();
+                    curPosFile2++;
+                    if (scanner2.hasNextLong()) a2 = scanner2.nextLong();
                 }
 
                 pw.flush();
--- src/lesson3/task5/Test.java
+++ src/lesson3/task5/Test.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        File dataFile = new Generator().generate("src/lesson3/task5/data.txt", 100);
+        File dataFile = new Generator().generate("src/lesson3/task5/data.txt", 51);
         System.out.println(new Validator(dataFile).isSorted());
         File sortedFile = new Sorter().sortFile(dataFile);
         System.out.println(new Validator(sortedFile).isSorted());
